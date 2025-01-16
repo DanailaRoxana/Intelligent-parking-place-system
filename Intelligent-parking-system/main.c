a4c00286aec9711abbd2ee45be47d44a966ad757
DanailaRoxana <danailaroxana2002@gmail.com>
2025-01-16 19:46:06 +0200

    add semaphore

--- Intelligent-parking-system/main.c
+++ Intelligent-parking-system/main.c
@@ -34,7 +34,7 @@ int main(void)
   TIMER_Start(&TIMER_ECHO_IN);
   TIMER_Start(&TIMER_ECHO_OUT);
 
-  xSemaphore = xSemaphoreCreateBinary();
+  xSemaphore = xSemaphoreCreateMutex();
   xQueue = xQueueCreate(5, sizeof(uint8_t));
 
   xTaskCreate(ProcessingTask, "Processing Task", 200, NULL, 2, NULL);
@@ -60,7 +60,6 @@ void ProcessingTask(void *pvParameters)
 
         printf("ProcessingTask duration: %lu us\n", (end_time - start_time)*1000);
 
-		vTaskDelay(pdMS_TO_TICKS(100));
 	}
 }
 
@@ -79,7 +78,6 @@ void CommunicationTask(void *pvParameters)
             if(occupied_spaces == PARK_SPACES)
             {
             	sprintf(message, "Entrance blocked!\n");
-            	printf("Generated message: %s", message);
             	vTaskDelay(pdMS_TO_TICKS(10));
                 UART_Transmit(&UART_BLUETOOTH, (uint8_t *)message, strlen(message));
             }
@@ -98,16 +96,19 @@ void check_entrance(void)
 	pulse_stop = echo_in_counter;
 
 	printf("distance_in = %lu\n", pulse_stop - pulse_start);
-//	printf("distance_in = %lu\n", echo_in_counter);
 
 	if ((pulse_stop - pulse_start) < 2 && last_distance_in >= 2)
 	{
 		printf("\nCar In detected\n");
-        if (parked_cars < PARK_SPACES)
-        {
-        	parked_cars++;
+		if (parked_cars < PARK_SPACES)
+		{
+			if (xSemaphoreTake(xSemaphore, portMAX_DELAY) == pdTRUE)
+			{
+				parked_cars++;
+				xSemaphoreGive(xSemaphore);
+			}
 			xQueueSend(xQueue, &parked_cars, portMAX_DELAY);
-        }
+		}
 	}
 
 	last_distance_in = pulse_stop - pulse_start;
@@ -124,14 +125,17 @@ void check_exit(void)
 	pulse_stop = echo_out_counter;
 
 	printf("distance_out = %lu\n", pulse_stop - pulse_start);
-//	printf("distance_out = %lu\n", echo_out_counter);
 
 	if ((pulse_stop - pulse_start) < 2 && last_distance_out >= 2)
 	{
 		printf("\nCar Out detected\n");
 		if (parked_cars > 0) {
-			parked_cars--;
-            xQueueSend(xQueue, &parked_cars, portMAX_DELAY);
+			if (xSemaphoreTake(xSemaphore, portMAX_DELAY) == pdTRUE)
+			{
+				parked_cars--;
+				xSemaphoreGive(xSemaphore);
+			}
+			xQueueSend(xQueue, &parked_cars, portMAX_DELAY);
 		}
 	}
 	last_distance_out = pulse_stop - pulse_start;
